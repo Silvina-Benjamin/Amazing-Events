920b3646596339cfe72ca3602b552788d40c52c5
Silvina Benjamin <silvina.benjamin@yahoo.com.ar>
2023-02-22 22:14:00 -0300

    responsive ok

---
+++ js/past.js
@@ -0,0 +1,9 @@
+for(let event of data.events){
+    let currentDate = new Date(data.currentDate);
+    let eventDate = new Date(event.date);
+
+    if (eventDate < currentDate) {
+        console.log("evento pasado: " + event.name)
+    }
+
+}
