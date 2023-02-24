199d3fa562297e89bdad93cb6742e10ca9c8c359
Silvina Benjamin <silvina.benjamin@yahoo.com.ar>
2023-02-24 19:19:19 -0300

    Terminado con las cards dinamicas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 for(let event of data.events){
 console.log(event.name);
+const contenedorOtraCard = document.createElement('div')
+contenedorOtraCard.classList.add('card')
+let otraCard = `<img src="${event.image}" class="card-img-top" alt="cinema">
+<div class="card-body">
+<h5 class="card-title">${event.name}</h5>
+<p class="card-text">${event.description}</p>
+<div class="fila-inferior-card">
+    <p>Price:<br> ${event.price}</p>
+    <a href="./details.html" class="btn btn-primary">Learn more</a>
+</div>
+</div>`
+contenedorOtraCard.innerHTML = otraCard
+console.log(contenedorOtraCard)
+const nuevoDiv = document.querySelector('div.contenedor-4-cards')
+document.querySelector('div.contenedor-4-cards').appendChild(contenedorOtraCard)
 }
--- js/upcoming.js
+++ js/upcoming.js
@@ -4,6 +4,22 @@ for(let event of data.events){
 
     if (eventDate > currentDate) {
         console.log("evento futuro: " + event.name)
+    const contenedorOtraCard = document.createElement('div')
+    contenedorOtraCard.classList.add('card')
+    let otraCard = `<img src="${event.image}" class="card-img-top" alt="cinema">
+    <div class="card-body">
+    <h5 class="card-title">${event.name}</h5>
+    <p class="card-text">${event.description}</p>
+    <div class="fila-inferior-card">
+        <p>Price:<br> ${event.price}</p>
+        <a href="./details.html" class="btn btn-primary">Learn more</a>
+    </div>
+    </div>`
+    contenedorOtraCard.innerHTML = otraCard
+    console.log(contenedorOtraCard)
+    const nuevoDiv = document.querySelector('div.contenedor-4-cards')
+    document.querySelector('div.contenedor-4-cards').appendChild(contenedorOtraCard)
+        
     }
-
 }
+        
