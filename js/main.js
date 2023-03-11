47972fa82d9815f82092a0399592fdab8d3b70df
silben90 <silvina.benjamin@yahoo.com.ar>
2023-03-11 18:44:20 -0300

    filtro por palabra casi listo

--- js/main.js
+++ js/main.js
@@ -30,11 +30,14 @@
     }
     contenedorCategorias.innerHTML = htmlCategorias
 
-    
-// Creo el filtro por categoria
-    
+// Creo el filtro por categoria y busqueda por palabra
     // variable que tendra todos los checkbox
     let casillasCheckbox = document.querySelectorAll('input[type=checkbox]')
+    // variable que corestpondera a lo que se escriba en la barra de búsqueda
+    let busqueda = document.querySelectorAll('input[type=search]')
+
+    // array de busqueda por palabra
+    let arrayDeBusqueda = data.events
  
     casillasCheckbox.forEach(input => {
         input.addEventListener('change', (e) =>{
@@ -48,4 +51,42 @@
             aplicoFiltro()
         })
     })
+
+    document.addEventListener('submit', (eb)=> {
+        eb.preventDefault()
+        let palabraBusqueda = eb.target[0].value.toLowerCase();
+        console.log("la palabra ingresada es " + palabraBusqueda)
+        let filtroBusqueda = data.events.filter (eventobusqueda => {    
+            if(eventobusqueda.name.toLowerCase().includes (palabraBusqueda) || eventobusqueda.description.toLowerCase().includes(palabraBusqueda )){
+                return true;
+            }
+            
+        })
+        console.log(filtroBusqueda)
+        for(let eB of filtroBusqueda){
+            contenedorPrincipal.innerHTML += crearCardParaEvento (eB)
+        }
+        })
+
+
+        
+// Creo el filtro por categoria
+    
+    // variable que tendra todos los checkbox
+    // let casillasCheckbox = document.querySelectorAll('input[type=checkbox]')
+ 
+    // casillasCheckbox.forEach(input => {
+    //     input.addEventListener('change', (e) =>{
+    //         const casilla = e.target;
+    //         if (casilla.checked){
+    //             categoriasSeleccionadas.push(casilla.value);  
+                
+    //         } else {
+    //             categoriasSeleccionadas = categoriasSeleccionadas.filter(categoria => categoria != casilla.value)
+    //         }
+    //         aplicoFiltro()
+    //     })
+    // })
+
+    // Creo el filtro para búsqueda por palabra
 
